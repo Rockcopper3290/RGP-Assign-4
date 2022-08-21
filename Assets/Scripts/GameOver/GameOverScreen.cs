0300756ae08a3baea020417d6a98e6eae0c13faa
Daelynn7036 <alexandra.s.drury@gmail.com>
2022-08-21 17:34:01 +1000

    [not working] Added Exit, Exit to Menu and play again Button to gameover screen

--- Assets/Scripts/GameOver/GameOverScreen.cs
+++ Assets/Scripts/GameOver/GameOverScreen.cs
@@ -10,20 +10,28 @@ public class GameOverScreen : MonoBehaviour
     // Set in Unity Inspector
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private TMP_Text highScoreText;
-    [SerializeField] private TMP_Text clickToReplayText;
+    //[SerializeField] private TMP_Text clickToReplayText;
     [SerializeField] private AudioSource music;
 
     // Game Over Screen Management
-    private float timeSinceStart;
+    //private float timeSinceStart;
 
-    public void PlayGame()
+    public void PlayGameNoTutorial()
     {
+        //splashMusic.Stop();
+
+        GameData.tutorial = false;
         SceneManager.LoadScene("Game");
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Splash");
     }
 
     // Awake is called before any Start methods, even if the GameObject is disabled
@@ -37,25 +45,25 @@ public class GameOverScreen : MonoBehaviour
     {
         this.scoreText.text = "SCORE: " + GameOverData.score.ToString();
         this.highScoreText.text = "HIGH SCORE: " + GameOverData.highScore.ToString();
-        this.clickToReplayText.text = "";
+        // this.clickToReplayText.text = "";
 
         music.Play();
 
-        this.timeSinceStart = 0.0f;
+        //this.timeSinceStart = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Increment Time Since Screen Start
-        this.timeSinceStart += Time.deltaTime;
+        //this.timeSinceStart += Time.deltaTime;
 
-        if (this.timeSinceStart >= 1.0f)
-        {
-            this.clickToReplayText.text = "CLICK TO REPLAY";
+        //if (this.timeSinceStart >= 1.0f)
+        //{
+        //    this.clickToReplayText.text = "CLICK TO REPLAY";
 
-            if (Input.anyKey || Input.anyKeyDown)
-                SceneManager.LoadScene("Game");
-        }
+        //    if (Input.anyKey || Input.anyKeyDown)
+        //        SceneManager.LoadScene("Game");
+        //}
     }
 }
